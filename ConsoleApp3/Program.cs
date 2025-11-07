7e914be3f75616e792860167b939f237f971530d
zsp3 <zsp3@DESKTOP-EHD8S3P>
2025-11-07 10:06:32 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Wpisz ocene:");
+string ocena = Console.ReadLine();
+switch(ocena)
+{
+    case "6":
+        Console.WriteLine("6 - Celujacy");
+        break;
+    case "5":
+        Console.WriteLine("5 - Bardzo dobry");
+        break;
+    case "4":
+        Console.WriteLine("4 - Dobry");
+        break;
+    case "3":
+        Console.WriteLine("3 - Dostateczny");
+        break;
+    case "2":
+        Console.WriteLine("2 - Dopuszczajacy");
+        break;
+    case "1":
+        Console.WriteLine("1 - Niedostateczny");
+        break;
+}
